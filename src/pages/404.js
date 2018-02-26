1b3e67c61f94b808fc0952a8c83a08169898ee43
Andrew Smith <kafie1985@hotmail.com>
2018-02-25 19:15:09 -0700

    update
    fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,7 @@ import { Container } from 'reactstrap'
 const NotFoundPage = () => (
   <Container>
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn't exist... the sadness.</p>
+    <p>You just hit a route that doesn't exist... Please hit back</p>
   </Container>
 )
 
